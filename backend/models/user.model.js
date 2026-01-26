af445ee2baa372703a7972b315252f7357712b59
Raifulalam <raifulalam0123@gmail.com>
2026-01-26 19:16:53 +0530

    updated

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -25,6 +25,6 @@ const userSchema = new mongoose.Schema(
 );
 
 // 🔥 Performance Index
-userSchema.index({ email: 1 });
+
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 export default User;
